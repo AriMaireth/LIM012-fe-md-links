8216ea8c97176d17f459d5f0ae4d1e69f5eb8f17
AriMaireth <ari.maireth@gmail.com>
2020-05-18 15:27:51 -0500

    test - manejo de ruta

--- src/index.js
+++ src/index.js
@@ -1,14 +1,45 @@
 // const os = require('os');
 const path = require('path');
-// const fs = require('fs');
-// const itsAbsolute = (route) => path.isAbsolute(route);
+const fs = require('fs');
 
+// Funcion para determinar si la ruta es absoluta y convertir rutas relativas a absolutas
 const itsAbsolute = (route) => {
   if (path.isAbsolute(route) === true) {
     return route;
   } return (path.resolve(route));
 };
 
+// Acá se comprueba si es una ruta de archivo
+const itsAFile = (fileRoute) => fs.statSync(fileRoute).isFile();
+// Devolver solo la extension.md de un archivo
+const fileExtension = (extension) => path.extname(extension) === '.md';
 
-console.log(itsAbsolute('C:/Users/galle/LIM012-fe-md-links/src/pruebaRuta/prueba.md'));
-console.log(itsAbsolute('./src/pruebaRuta/prueba.js'));
+// Recorrer directorio hasta encontrar archivos y sacar solo su extension .md
+const searchInDirectory = (route) => {
+  const convertToAbsolutePath = itsAbsolute(route);
+  let mdFile = [];
+  if (itsAFile(convertToAbsolutePath)) {
+    if (fileExtension(convertToAbsolutePath)) {
+      mdFile.push(convertToAbsolutePath);
+    }
+  } else {
+    const readExtension = fs.readdirSync(convertToAbsolutePath);
+    readExtension.forEach((extension) => {
+      mdFile = mdFile.concat(searchInDirectory(path.join(convertToAbsolutePath, extension)));
+    });
+  }
+  return mdFile;
+};
+module.exports = {
+  itsAbsolute, itsAFile, fileExtension, searchInDirectory,
+};
+
+
+// console.log(searchInDirectory('C:/Users/galle/LIM012-fe-md-links/src/pruebaRuta'));
+// console.log(searchInDirectory('./src'));
+
+// const itsAFile = (fileRoute) => {
+//   if (fs.statSync(fileRoute).isfile === true) {
+//     return route;
+// }else {}
+// }
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,5 +1,53 @@
-// require('./src/index');
+const {
+  itsAbsolute, itsAFile, fileExtension, searchInDirectory,
+} = require('../src/index.js');
 
-// describe('', () => {
-//   it('');
-// });
+describe('itsAbsolute', () => {
+  it('itsAbsolute es una funcion', () => {
+    expect(typeof itsAbsolute).toBe('function');
+  });
+  it('retorna todas las rutas convertidas a absolutas', () => {
+    const ruta = './src/pruebaRuta/prueba.js';
+    const rutaAbsoluta = 'C:\\Users\\galle\\LIM012-fe-md-links\\src\\pruebaRuta\\prueba.js';
+    expect(itsAbsolute(ruta)).toMatch(rutaAbsoluta);
+  });
+});
+
+describe('itsAFile', () => {
+  it('itsAFile es una funcion', () => {
+    expect(typeof itsAFile).toBe('function');
+  });
+  it('Retorna true para los archivos', () => {
+    const ruta = './src/pruebaRuta/prueba.js';
+    expect(itsAFile(ruta)).toBe(true);
+  });
+  it('Deberia retornar false si no es archivo', () => {
+    const ruta = 'C:/Users/galle/LIM012-fe-md-links/src/pruebaRuta';
+    expect(itsAFile(ruta)).toBe(false);
+  });
+});
+
+describe('fileExtension', () => {
+  it('fileExtension es una funcion', () => {
+    expect(typeof fileExtension).toBe('function');
+  });
+  it('Retorna true si la extension del archivo es MD', () => {
+    const ruta = './src/pruebaRuta/prueba.md';
+    expect(fileExtension(ruta)).toBe(true);
+  });
+  it('Retorna false si la extension del archivo no es MD', () => {
+    const ruta = './src/pruebaRuta/prueba.js';
+    expect(fileExtension(ruta)).toBe(false);
+  });
+});
+
+describe('searchInDirectory', () => {
+  it('searchInDirectory es una funcion', () => {
+    expect(typeof searchInDirectory).toBe('function');
+  });
+  it('Deberia recorrer el directorio y retornar un array con solo los archivos MD', () => {
+    const ruta = './src/pruebaRuta';
+    const resultado = ['C:\\Users\\galle\\LIM012-fe-md-links\\src\\pruebaRuta\\prueba.md'];
+    expect(searchInDirectory(ruta)).toStrictEqual(resultado);
+  });
+});
